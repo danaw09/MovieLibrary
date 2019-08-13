b4b3be8decf01dd21ee44bfe75d34932d1868c0a
dana <danaw09@gmail.com>
2019-08-13 12:07:30 -0500

    initial

--- MovieLibrary/MovieLibrary/Global.asax.cs
+++ MovieLibrary/MovieLibrary/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
 
 namespace MovieLibrary
 {
--- MovieLibrary/MovieLibrary/Properties/AssemblyInfo.cs
+++ MovieLibrary/MovieLibrary/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e711d94-c1b7-4afb-a1de-c31131c0cce1")]
+[assembly: Guid("11888f20-3b58-4fa4-a60c-38cd9427e591")]
 
 // Version information for an assembly consists of the following four values:
 //
